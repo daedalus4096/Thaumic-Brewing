8abcd4a99a988b3f8eaf3df85513b258ed851bb7
Michael Bunting <daedalus4096@gmail.com>
2019-01-11 23:47:08 -0800

    Initial commit

---
+++ src/main/java/com/verdantartifice/thaumicbrewing/ThaumicBrewing.java
@@ -0,0 +1,31 @@
+package com.verdantartifice.thaumicbrewing;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ThaumicBrewing.MODID, name = ThaumicBrewing.NAME, version = ThaumicBrewing.VERSION)
+public class ThaumicBrewing
+{
+    public static final String MODID = "thaumic-brewing";
+    public static final String NAME = "Thaumic Brewing";
+    public static final String VERSION = "0.0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
